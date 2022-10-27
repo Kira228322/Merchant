7b9edde697ffe3d3486547bcf0603f8b06b6cf55
Maher <foxi-yaksuns@mail.ru>
2022-10-27 17:29:41 +0300

    Доработал инспектор Merchant'a теперь красиво и вроде корректно работает все. Залил ассеты (преимущественно для UI)

--- Assets/KirillTMP/Merchant.cs
+++ Assets/KirillTMP/Merchant.cs
@@ -10,16 +10,20 @@ public class Merchant : MonoBehaviour
     // каждый торговец будет иметь свою цену на этот товар. ИБО В ЭТОМ И СМЫСЛ ИГРЫ епт
     
     public List<Item> Goods;
-    // public List<int> NewPrice;
-    // public List<int> CountOfGood;
-    private int[] _newPrice;
-    private int[] _countOfGood;
+    [HideInInspector] public List<Item> MerchantGoods;
+
+    [HideInInspector] public List<int> NewPrice;
+    [HideInInspector] public List<int> CountOfGood;
+    // private int[] _newPrice;
+    // private int[] _countOfGood;
 
     private void Start()
     {
+        MerchantGoods = new List<Item>();
         for (int i = 0; i < Goods.Count; i++)
         {
-            Goods[i].Price = _newPrice[i];
+            MerchantGoods.Add(Instantiate(Goods[i])); // Важно продублировать объект типа ScriptableObject, тк иначе
+            MerchantGoods[i].Price = NewPrice[i]; // он будет ссылаться на оригинал и изменять его понял. нужен дубликат
         }
     }
 
@@ -27,7 +31,7 @@ public class Merchant : MonoBehaviour
 
 
     [CustomEditor(typeof(Merchant))]
-    public class MerchantEditor : Editor // когда-то ты меня научил этому 
+    public class MerchantEditor : Editor 
     {
         public override void OnInspectorGUI()
         {
@@ -35,21 +39,38 @@ public class Merchant : MonoBehaviour
             
             
             Merchant merchant = (Merchant)target;
-
-            merchant._newPrice = new int[merchant.Goods.Count];
-            merchant._countOfGood = new int[merchant.Goods.Count];
+            
+            
+            
+            
+            while (merchant.NewPrice.Count < merchant.Goods.Count)
+            {
+                merchant.NewPrice.Add(0);
+                merchant.CountOfGood.Add(0);
+            }
+            while (merchant.NewPrice.Count > merchant.Goods.Count)
+            {
+                merchant.NewPrice.RemoveAt(merchant.NewPrice.Count-1);
+                merchant.CountOfGood.RemoveAt(merchant.CountOfGood.Count-1);
+            }
+            
             for (int i = 0; i < merchant.Goods.Count; i++)
             {
+                if (merchant.Goods[i] == null)
+                    continue;
                 EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.LabelField(merchant.Goods[i].Name, GUILayout.MaxWidth(110));
+                EditorGUILayout.Space(-18, true);
+                EditorGUILayout.LabelField(merchant.Goods[i].Name, GUILayout.MaxWidth(90));
+                EditorGUILayout.Space(1, true);
+                EditorGUILayout.LabelField(merchant.Goods[i].Price + "-AvgPrice", GUILayout.MaxWidth(75));
                 
-                EditorGUILayout.Space(3, true);
-                EditorGUILayout.LabelField("Price", GUILayout.MaxWidth(40));
-                merchant._newPrice[i] = EditorGUILayout.IntField(merchant._newPrice[i]);
+                EditorGUILayout.Space(2, true);
+                EditorGUILayout.LabelField("Price", GUILayout.MaxWidth(31));
+                merchant.NewPrice[i] = EditorGUILayout.IntField(merchant.NewPrice[i]);
                 
-                EditorGUILayout.Space(3, true);
-                EditorGUILayout.LabelField("Count", GUILayout.MaxWidth(40));
-                merchant._countOfGood[i] = EditorGUILayout.IntField(merchant._countOfGood[i]);
+                EditorGUILayout.Space(2, true);
+                EditorGUILayout.LabelField("Count", GUILayout.MaxWidth(36.5f));
+                merchant.CountOfGood[i] = EditorGUILayout.IntField(merchant.CountOfGood[i]);
                 EditorGUILayout.EndHorizontal();
             }
         }
