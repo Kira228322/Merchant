4963321df677eb2b9c376de0a690f388546276e6
Maher <foxi-yaksuns@mail.ru>
2022-11-27 19:55:51 +0300

    Поработал с мувом. Скрипт стал получше. Поусточвее: нельзя во время прыжка менять направление, есть кд на движение Но все-таки иногда можно сломать логику движения. Не очень понимаю как это происходит, но связано с прыжками, когда пытаешь изменить движение перед прышком или после. Хотя проверка на то, находишься ты в прыжке или нет есть, почему-то не всегда она работает... Сломать движение довольно трудно, надо постоянно кликать в разные стороны от игрока + делать это довольно часто, что как бы не предполагается в обычном поведении игрока + наблюдается только если есть много лестниц, когда лестница одна все вроде работает нормально. Если надо будет доработать эту тему, постараюсь, хотя провозился с этой штукой и не понимаю почему да как

--- Assets/KirillTMP/PlayerMover.cs
+++ Assets/KirillTMP/PlayerMover.cs
@@ -14,27 +14,45 @@ public class PlayerMover : MonoBehaviour
     private float _minDistToLet;
     private Coroutine _currentMove;
     private float _lastValue;
+
+    private float _moveCD = 0.2f;
+    private float _currentMoveCD;
+    private const float MinConstDist = 0.2f; // минимальное расстояние, которое может возникнуть между игроком и стеной 
     private void Start()
     {
         _collider = GetComponent<Collider2D>();
         _rigidbody = GetComponent<Rigidbody2D>();
-        _minDistToLet = 0.2f + _collider.bounds.size.x/2;
+        _minDistToLet = MinConstDist + _collider.bounds.size.x/2; 
         _lastValue = transform.position.x;
-    }
-
-    public void StopMove()
-    {
-        StopCoroutine(_currentMove);
     }
 
     public void StartMove(Vector3 startPos,Vector3 targetPos)
     {
-        if (_currentMove != null)
+        if (_currentMoveCD == 0)
         {
-            StopCoroutine(_currentMove);
-            _currentMove = null;
+            _currentMoveCD = _moveCD;
+            StartCoroutine(ResetMoveCD());
+            
+            if (_currentMove != null)
+            {
+                StopCoroutine(_currentMove);
+                Debug.Log("Break");
+                _currentMove = null;
+            }
+
+            if (_collider.IsTouchingLayers()) // двигаться можно, только если ты не в прыжке
+            {
+                _currentMove = StartCoroutine(Move(startPos, targetPos));
+                Debug.Log("Start");
+            }
         }
-        _currentMove = StartCoroutine(Move(startPos ,targetPos));
+    }
+
+    private IEnumerator ResetMoveCD()
+    {
+        WaitForSeconds waitForSeconds = new WaitForSeconds(_moveCD);
+        yield return waitForSeconds;
+        _currentMoveCD = 0;
     }
     
     public IEnumerator Move(Vector3 startPos,Vector3 targetPos)
@@ -42,7 +60,7 @@ public class PlayerMover : MonoBehaviour
         bool moveIsDone = false;
         WaitForSeconds waitForSeconds = new WaitForSeconds(0.02f);
         RaycastHit2D[] raycastHit2D = new RaycastHit2D[8];
-        
+
         Vector2 castDirection = new Vector3(targetPos.x - startPos.x, 0.05f); //+0.05y,чтобы не коллизился с полом
         int count;
         if (_rigidbody.Cast(castDirection, _contactFilter2D, raycastHit2D, 
@@ -54,12 +72,13 @@ public class PlayerMover : MonoBehaviour
             {
                 transform.position = new Vector3(math.lerp(startPos.x, targetPos.x, i/count), transform.position.y);
                 yield return waitForSeconds;
+                    // проверка если игрок спуститься вниз, то y поменяется и нужно снова отслеживать препятствие
                 if (i % 5 == 0)
-                    if (_rigidbody.Cast(castDirection, _contactFilter2D, raycastHit2D,
+                    if (_rigidbody.Cast(castDirection, _contactFilter2D, raycastHit2D, 
                             Math.Abs(targetPos.x - transform.position.x) + _minDistToLet) != 0)
                     {
-                        if (raycastHit2D[0].distance > 0.01f)
-                        {
+                        if (raycastHit2D[0].distance > 0.01f) // Это надо, чтобы при спуске вниз, когда объект касается с
+                        { // землей не ломалось движение 
                             StartCoroutine(Move(transform.position, targetPos));
                             break;
                         }
@@ -69,16 +88,14 @@ public class PlayerMover : MonoBehaviour
         else 
         {
             Vector2 jumpDirection;
-            float distToLet = raycastHit2D[0].distance - 0.2f;
+            float distToLet = raycastHit2D[0].distance - MinConstDist; 
 
             count = Convert.ToInt32(Math.Abs(distToLet) / (_speed * 0.02f));
-
-            //
+            
             float targetPosX = raycastHit2D[0].point.x;
             if (targetPosX > startPos.x)
                 targetPosX -= _minDistToLet;
             else targetPosX += _minDistToLet;
-            //
 
             for (float i = 1; i <= count; i++)
             {
@@ -102,14 +119,15 @@ public class PlayerMover : MonoBehaviour
                 waitForSeconds = new WaitForSeconds(0.08f); // задержка перед подпрыгиванием 
                 yield return waitForSeconds;
                 castDirection = new Vector2(raycastHit2D[0].point.x - transform.position.x, 0).normalized;
-                castDirection += new Vector2(0, 2.5f); // кастыль
-                if (_rigidbody.Cast(castDirection, _contactFilter2D, raycastHit2D, _minDistToLet) == 0)
+                castDirection += new Vector2(0, 2f); // max высота ступеньки, на которую может
+                // прыгнуть игрок * (1/MinConstDist) ; пусть высота = 0.4, тогда y = 2
+                if (_rigidbody.Cast(castDirection, _contactFilter2D, raycastHit2D, _minDistToLet) == 0) 
                 {
                     if (raycastHit2D[0].point.x > transform.position.x)
-                        jumpDirection = new Vector2(0.342f, 0.939f); // 70 градусов
-                    else jumpDirection = new Vector2(-0.342f, 0.939f);
+                        jumpDirection = new Vector2(0.34f, 0.94f); // 70 градусов
+                    else jumpDirection = new Vector2(-0.34f, 0.94f);
 
-                    _rigidbody.AddForce(jumpDirection * 185);
+                    _rigidbody.AddForce(jumpDirection * 180);
                 }
 
                 
@@ -120,12 +138,11 @@ public class PlayerMover : MonoBehaviour
                 }
                 
                 if (Math.Abs(transform.position.x - targetPos.x) > 0.4f && _lastValue != transform.position.x)
-                {
-                    _lastValue = transform.position.x;
+                { // (transform.position.x - targetPos.x) > 0.4f - небольшая погрешность, на всякий случай
+                    _lastValue = transform.position.x; // надо если игрок задал движение в недостижимую точку
                     StartCoroutine(Move(transform.position, targetPos));
                 }
             }
         }
-        _currentMove = null;
     }
 }
