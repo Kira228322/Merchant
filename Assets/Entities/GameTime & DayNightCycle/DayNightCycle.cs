30ead5240f2a98852998d6dee2bd9c7cb0c85552
Maher <foxi-yaksuns@mail.ru>
2023-04-29 22:02:38 +0300

    Сделал анимацию для источников света.
    Была возможность сделать анимацию костра или любого другого источника света через Шум Перлина. Но я поразбирался, подумал, лучше подогнать функции какие-то, чтобы вышло поэкономнее поресурсам. С шумом было бы намного естественнее, реалистичнее, красивее, но затратнее. Если не прям чтобы пялить, то выглядит нормально.
    Важно не выставлять большой диапозон для интенсивности!
    Хотел бахнуть механику костра, но там требует совета с тобой.

--- Assets/Entities/GameTime & DayNightCycle/DayNightCycle.cs
+++ Assets/Entities/GameTime & DayNightCycle/DayNightCycle.cs
@@ -62,26 +62,22 @@ public class DayNightCycle : MonoBehaviour
         switch (GameTime.Hours)
         {
             case int n when n >= 22 || n <= 2:
-                Debug.Log("1");
                 volumeWeight = 1;
                 break;
             case int n when n >= 3 && n <= 11:
-                Debug.Log("2");
                 cosineValue = ((GameTime.Hours-3) * 60 + GameTime.Minutes) / 540f; // изменяется от 0 до 1, когда время изменяется от 3 до 12
                 volumeWeight = Mathf.Cos(cosineValue * Mathf.PI) * 0.5f + 0.5f; // см f(x) = cos(Px)/2 + 0.5f
                 break;
             case int n when n >= 12 && n <= 16:
-                Debug.Log("3");
                 volumeWeight = 0;
                 break;
             case int n when n >= 17 && n <= 21:
-                Debug.Log("4");
                 cosineValue = 1 - ((GameTime.Hours - 17) * 60 + GameTime.Minutes) / 300f; // изменяется от 1 до 0, когда время изменяется от 17 до 22
                 volumeWeight = Mathf.Cos(cosineValue * Mathf.PI) * 0.5f + 0.5f;
                 break;
         }
         _volume.weight = volumeWeight + _rainWeightOffset;
-        _sun.intensity = Mathf.Lerp(0.2f, 0.97f, 1-volumeWeight); // 0.2 и 0.97 это min и max значения которыми может быть освещение
+        _sun.intensity = Mathf.Lerp(0.2f, 0.96f, 1-volumeWeight); // 0.2 и 0.96 это min и max значения которыми может быть освещение
         if (_volume.weight >= 1) _volume.weight = 1;
         if (!_activateLights)
         {
