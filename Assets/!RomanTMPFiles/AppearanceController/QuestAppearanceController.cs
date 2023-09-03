647b7b726cd275f23dc8fa28f129b9b4aac50c21
Bogoya <bogoya82@gmail.com>
2023-09-03 11:00:27 +0300

    Сделал контроллер появления и исчезновения объектов в зависимости от выполненных/проваленных квестов
    Переместил завершенные элементы из TMP папки в общие

---
+++ Assets/!RomanTMPFiles/AppearanceController/QuestAppearanceController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestAppearanceController : MonoBehaviour
+{
+    [Serializable]
+    private class AppearanceControls
+    {
+        public string QuestSummary;
+        public Quest.State questState;
+        
+        public bool IsReady()
+        {
+            Quest quest = QuestHandler.GetQuestBySummary(QuestSummary);
+            if (quest == null)
+                return false;
+            if (questState == quest.CurrentState)
+                return true;
+            if (questState == Quest.State.Completed && quest.CurrentState == Quest.State.RewardUncollected)
+                return true;
+            return false;
+        }
+    }
+    private enum Appearance { Show, Hide }
+    [SerializeField] private Appearance _appearance;
+
+    [SerializeField] private List<AppearanceControls> _controls = new();
+
+    private void Start()
+    {
+        CheckAppearance();
+    }
+
+    private void CheckAppearance()
+    {
+        foreach (var control in _controls)
+        {
+            if (!control.IsReady())
+            {
+                return;
+            }
+        }
+
+
+        bool shouldShow = _appearance == Appearance.Show;
+        gameObject.SetActive(shouldShow);
+    }
+}
