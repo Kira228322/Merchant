a58db8dd3957cf30ffcfb1a8748522c945254114
Bogoya <bogoya82@gmail.com>
2024-03-01 17:10:59 +0300

    Сделали первый бета-выпуск игры для закрытого тестирования Вернули Target Platform как андроид и сделал соответствующие настройки в Project Settings
    Загрузили иконку игры в папку Assets
    
    Сделал окружение для особой сцены23 и настроил скрипт для концовки игры

---
+++ Assets/!RomanTMPFiles/MainStoryFinisher.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainStoryFinisher : MonoBehaviour
+{
+    // Скрипт который перекидывает на другую сцену после активации устройства по мейн сюжету
+
+    private void OnEnable()
+    {
+        QuestHandler.QuestChangedState += OnQuestChangedState;
+    }
+
+    private void OnDisable()
+    {
+        QuestHandler.QuestChangedState += OnQuestChangedState;
+    }
+
+    private void OnQuestChangedState(Quest quest)
+    {
+        if (quest.QuestSummary == "activate_device" && quest.CurrentState == Quest.State.Completed)
+        {
+            FindObjectOfType<SceneTransiter>().StartTransit(MapManager.GetLocationBySceneName("Scene20"));
+        }
+    }
+
+}
