0f7359eabe8d6f2280ebf9995f91a3117bc78005
Bogoya <bogoya82@gmail.com>
2025-10-12 18:40:47 +0300

    Провел большую работу по удалению английского текста

--- Assets/Entities/CooldownHandler/CooldownHandler.cs
+++ Assets/Entities/CooldownHandler/CooldownHandler.cs
@@ -78,14 +78,12 @@ public class CooldownHandler : MonoBehaviour, ISaveable<CooldownHandlerSaveData>
 
     public CooldownHandlerSaveData SaveData()
     {
-        Debug.Log(ObjectsOnCooldown.Count);
         CooldownHandlerSaveData saveData = new(this);
         return saveData;
     }
 
     public void LoadData(CooldownHandlerSaveData data)
     {
-        Debug.Log(data.ObjectsOnCooldown);
         ObjectsOnCooldown.AddRange(data.ObjectsOnCooldown);
     }
 }
